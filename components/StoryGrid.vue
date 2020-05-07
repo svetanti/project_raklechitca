0ca109e0d51fd96d6c2901136055d5c8bd4a0f25
murbook <dimamurugov@gmail.com>
2020-05-07 22:37:46 +0300

    add block Stories

---
+++ components/StoryGrid.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="containerStory">
+    <my-Card />
+    <my-Card />
+    <my-Card />
+    <my-Card />
+    <my-Card />
+    <my-Card />
+    <my-Card />
+    <my-Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/ui/Card';
+
+export default {
+  components: {
+    'my-Card': Card,
+  },
+};
+</script>
+
+<style scoped>
+.containerStory {
+  display: grid;
+  grid-template-rows: repeat(2, calc((100% - 120px) / 2));
+  grid-template-columns: repeat(4, calc((100% - 70px) / 4));
+  row-gap: 70px;
+  column-gap: 40px;
+  max-width: 1320px;
+}
+</style>
--- components/ui/card.vue
+++ components/ui/card.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    Карточка конкретной истории
+  <div class="card">
+    <img
+      class="card__photo"
+      src="../../static/images/card__photo/photo1.jpg"
+      alt="Фото автора"
+    />
+    <h3 class="card__title">Владимир Тен</h3>
+    <p class="card__subtitle">
+      Я всегда читаю книги с конца, - и это не лечится, в отличие от рака.
+    </p>
   </div>
 </template>
 
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.card {
+  max-width: 300px;
+  font-family: 'Inter';
+}
+.card__photo {
+  max-width: 300px;
+  max-height: 300px;
+  object-fit: cover;
+  margin-bottom: 20px;
+}
+.card__title {
+  font-style: normal;
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 22px;
 
+  text-align: left;
+  margin-bottom: 14px;
+}
+.card__subtitle {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 18px;
+
+  text-align: left;
+  color: #666666;
+  max-width: 250px;
+}
 </style>
