403cedc62a32d1dac132846fef82388d97997fb5
Svetlana Antipova <die-chimaere@yandex.ru>
2020-05-07 14:35:27 +0300

    fix: исправить закрывающий тег в Index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <header><header>
+    <header></header>
     <h1>Главная страница</h1>
     <a href="http://localhost:3000/stories">страница со всеми историями</a>
     <a href="http://localhost:3000/stories/1">страница с конкретной историей</a>
