0578d1de362d46481cbbe07f0d10746ddb3060ca
ncaatl <alncaatl@gmail.com>
2020-05-12 01:41:59 +0300

    блоки Инстаграм и Рассажите-свою-историю - адаптив полностью

--- components/Instagram.vue
+++ components/Instagram.vue
@@ -6,9 +6,7 @@
     </h3>
     <div class="content-container">
       <div class="text-content">
-        <p class="subtitle subtitle__underline">
-          <span class="subtitle subtitle__text">Инстаграм</span>
-        </p>
+        <p class="subtitle">Инстаграм</p>
         <p class="paragraph">
           Два раза в неделю мы просматриваем все посты по хештегу #этонелечится.
           Все истории, где нет нецензурных выражений и запрещенного контента
@@ -71,26 +69,13 @@ export default {
 }
 
 .subtitle {
-  margin-bottom: 30px;
-}
-
-.subtitle__underline {
-  font-size: 34px;
-  background: linear-gradient(
-    to top,
-    black,
-    black 4px,
-    transparent 4px,
-    transparent
-  );
-  width: fit-content;
-}
-
-.subtitle__text {
   font-weight: 600;
   font-size: 32px;
   line-height: 36px;
   color: black;
+  margin-bottom: 30px;
+  border-bottom: 3px solid currentColor;
+  width: max-content;
 }
 
 .paragraph {
@@ -116,7 +101,7 @@ export default {
   float: left;
 }
 
-@media screen and (max-width: 1399px) {
+@media screen and (min-width: 1280px) and (max-width: 1439px) {
   .instagram {
     max-width: 1180px;
   }
@@ -133,9 +118,10 @@ export default {
     margin: 90px 0;
   }
 
-  .subtitle__text {
+  .subtitle {
     font-size: 28px;
     line-height: 32px;
+    margin-top: 10px;
   }
 
   .paragraph {
@@ -146,6 +132,176 @@ export default {
   .insta-grid {
     width: 65%;
     grid-gap: 27px;
+  }
+}
+
+@media screen and (min-width: 1023px) and (max-width: 1279px) {
+  .instagram {
+    max-width: 924px;
+  }
+
+  .instagram-header {
+    font-size: 24px;
+    line-height: 40px;
+  }
+
+  .instagram-header-tag {
+    font-size: 35px;
+  }
+
+  .content-container {
+    margin: 80px 0;
+  }
+
+  .text-content {
+    width: 28%;
+  }
+
+  .subtitle {
+    margin-top: 10px;
+    margin-bottom: 15px;
+    font-size: 24px;
+    line-height: 28px;
+  }
+
+  .paragraph {
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .insta-grid {
+    grid-template-columns: repeat(auto-fit, minmax(135px, 1fr));
+    grid-gap: 20px;
+  }
+}
+
+@media screen and (min-width: 730px) and (max-width: 1022px) {
+  .instagram {
+    width: 90%;
+    max-width: 688px;
+  }
+
+  .instagram-header {
+    font-size: 22px;
+    line-height: 34px;
+  }
+
+  .instagram-header-tag {
+    font-size: 32px;
+    line-height: 30px;
+  }
+
+  .content-container {
+    flex-direction: column;
+    margin: 80px 0;
+    align-items: center;
+  }
+
+  .text-content {
+    width: 56%;
+    margin-bottom: 60px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .subtitle {
+    margin-bottom: 23px;
+    font-size: 24px;
+    line-height: 28px;
+    text-align: center;
+  }
+
+  .paragraph {
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .insta-grid {
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+  }
+}
+
+@media screen and (min-width: 400px) and (max-width: 729px) {
+  .instagram {
+    width: 90%;
+  }
+
+  .instagram-header {
+    font-size: 22px;
+    line-height: 30px;
+  }
+
+  .instagram-header-tag {
+    font-size: 30px;
+    line-height: 40px;
+  }
+
+  .content-container {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .text-content {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 50px;
+  }
+
+  .insta-grid {
+    width: 100%;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+  }
+}
+
+@media screen and (max-width: 399px) {
+  .instagram {
+    width: 90%;
+    min-width: 240px;
+  }
+
+  .instagram-header {
+    font-size: 16px;
+    line-height: 18px;
+    padding: 10px;
+  }
+
+  .instagram-header-tag {
+    font-size: 22px;
+    line-height: 26px;
+  }
+
+  .content-container {
+    flex-direction: column;
+    margin: 60px 0;
+  }
+
+  .text-content {
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
+  .subtitle {
+    font-size: 18px;
+    line-height: 21px;
+    border-bottom: 2px solid currentColor;
+    margin-bottom: 15px;
+  }
+
+  .paragraph {
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .insta-grid {
+    width: 100%;
+    grid-template-columns: repeat(auto-fit, minmax(120px, 1fr));
+    grid-gap: 10px;
   }
 }
 </style>
